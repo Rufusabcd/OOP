9a907a30841ce05d58b2d601ae5db9ad5e0432c9
Marcin Grzeniewski <MarcinGrzeniewski@MacBook-Air-Marcin.local>
2017-10-11 21:03:36 +0200

    add

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function Phone(brand, price, color) {
+function Phone(brand, price, color, cond) {
     this.brand = brand;
 	this.price = price;
-	this.color = color;
+    this.color = color;
+    this.cond = cond;
 }
 
-Phone.prototype.printinfo = function() {
-    console.log("The phone brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+Phone.prototype.printInfo = function() {
+    console.log("The phone is " + this.cond + " it brand is " + this.brand + ", color is " + this.color + " and the price is " + this.price + ".");
+}
+Phone.prototype.usefulInfo = function() {
+    console.log("" + this.brand + " " + this.cond + " phones usually costs around " + this.price + "");
 }
 
-var iPhone6s = new Phone("Apple", 2200, "red");
-var SamsungGalaxyS6 = new Phone("Samsung", 2000, "black");
-var OnePlus = new Phone("OnePlus", 2500, "silver")
+
+var iPhone6s = new Phone("Apple", 2200, "red", "new");
+var SamsungGalaxyS6 = new Phone("Samsung", 2000, "black", "used");
+var OnePlus = new Phone("OnePlus", 2500, "silver", "new")
